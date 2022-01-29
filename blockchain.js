7327825d791bf5869683b354e7221af9bb486f55
mfmeza231 <mfmeza231@gmail.com>
2022-01-28 18:07:44 -0800

    Blockchain average work and difficulty adjuster

--- blockchain.js
+++ blockchain.js
@@ -1,5 +1,5 @@
 const Block = require('./block')
-const cryptHash = require('./crypto-hash')
+const cryptoHash = require('./crypto-hash')
 class Blockchain {
     constructor() {
         this.chain = [Block.genesis()]
@@ -34,14 +34,17 @@ class Blockchain {
             return false;
         }
         for (let i=1; i<chain.length; i++) {
-            const { timestamp, lastHash, hash, data } = chain[i];
+            const { timestamp, lastHash, hash, nonce, difficulty, data } = chain[i];
             const actualLastHash = chain[i-1].hash;
+            const lastDifficulty = chain[i-1].difficulty;
             
             if (lastHash !== actualLastHash) return false;
 
-            const validatedHash = cryptHash(timestamp, lastHash, data);
+            const validatedHash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
 
             if (hash !== validatedHash) return false;
+
+            if (Math.abs(lastDifficulty - difficulty > 1)) return false;
         }
         return true;
     }
